ab74a747e68812efc57d01092e8276d38847ae4a
kadariyaujwal <cooloozewall@gmail.com>
2018-12-26 16:17:59 +0545

    Event emitter from component to parent element

--- js/app.js
+++ js/app.js
@@ -1,63 +1,21 @@
-Vue.component('tabs', {
-    template: `
-        <div>
-            <div class="tabs">
-                <ul>
-                    <li v-for="tab in tabs" :class="{ 'is-active' : tab.isActive}">
-                        <a :href="tab.href" @click="selectTab(tab)">{{tab.name}}</a>
-                    </li>
-                </ul>
-            </div>
-
-            <div class="tab-details">
-                <slot></slot>
-            </div>
-        </div>
+Vue.component('coupoun',{
+    template:`
+        <input class="form-control" placeholder = "please enter your coupoun code" @blur="onCoupounApplied">
     `,
-    data(){
-        return{
-            tabs:[]
-        }
-    },
-    created(){
-        this.tabs=this.$children;
-    },
     methods:{
-        selectTab(currentTab){
-            this.tabs.forEach(function(tab){
-                tab.isActive = false;
-            });
-            currentTab.isActive=true;
+        onCoupounApplied(){
+            this.$emit('applied');
         }
-    }
-});
-Vue.component('tab', {
-    template: `
-        <div v-show="isActive">
-            <slot></slot>
-        </div>
-    `,
-    data() {
-        return {
-          isActive:false,  //You should consider props to be immutable(meaning it cannot be changed), so make it mutable with data() or computed properties of vue.
-        }
-    },
-    mounted(){
-        this.isActive = this.selected; 
-    },
-    computed:{
-        href(){
-            return "#"+this.name.toLowerCase().replace(/ /g,'-'); 
-        }
-    },
-    props:{
-        name:{required:true},
-        selected:{default:false}
     }
 })
 new Vue({
     el: '#app',
     data: {
-        selected: true,
+        coupounApplied:false
+    },
+    methods:{
+        onCoupounApplied(){
+            this.coupounApplied = true;
+        }
     }
 })
