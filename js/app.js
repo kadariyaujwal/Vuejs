3effa8b882bc9e6b3d6ccfb2487adbaa6c6bf721
kadariyaujwal <cooloozewall@gmail.com>
2018-12-26 13:40:46 +0545

    emit custom events between components to change the data in root element

--- js/app.js
+++ js/app.js
@@ -1,24 +1,20 @@
-Vue.component('message', {
-    props:['title','body']
-    ,
-    data(){
-        return {
-            isVisible:true,
-        }
-    },
+Vue.component('modal', {
     template: `
-    <article class="message" v-show="isVisible">
-    <div class="message-header">
-        <p>{{title}}</p>
-        <button class="delete" aria-label="delete" @click="isVisible = false"></button>
-    </div>
-    <div class="message-body">
-        {{body}}
-    </div>
-    </article> 
+        <div class="modal is-active">
+            <div class="modal-background"></div>
+            <div class="modal-content">
+                <div class="box">
+                    <slot></slot>
+                </div>
+            </div>
+            <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
+        </div>
     `
 })
 
 new Vue({
-    el: '#app'
+    el: '#app',
+    data:{
+        showModal:false
+    }
 })
