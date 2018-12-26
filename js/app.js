57334110a50da322236d083e3a8129eccb970d9e
kadariyaujwal <cooloozewall@gmail.com>
2018-12-26 12:47:35 +0545

    vue component within another component

--- js/app.js
+++ js/app.js
@@ -1,38 +1,29 @@
-var app = new Vue({
-    el:'#app',
-    data:{
-     tasks:[
-         {description:"go to the store",completed:true},
-         {description:"play music",completed:false},
-         {description:"eat the apple",completed:true},
-         {description:"do some code",completed:false},
-         {description:"dance",completed:true},
-         {description:"do the work",completed:false},
-         {description:"submit assignment",completed:false},
-         {description:"visit college",completed:false},
-     ]
-    },
-    method:{
-        toggleCompleted(task){
-            
+Vue.component('task-list',{
+    template:
+    `           <div>
+                    <task v-for="task in tasks" v-text="task.description" :key="task.id"></task>
+                </div>
+    `,
+    data(){
+        return{
+            tasks:[
+                {description:"go to the store",completed:true},
+                {description:"play music",completed:false},
+                {description:"eat the apple",completed:true},
+                {description:"do some code",completed:false},
+                {description:"dance",completed:true},
+                {description:"do the work",completed:false},
+                {description:"submit assignment",completed:false},
+                {description:"visit college",completed:false},
+            ]
         }
-    },
-    computed:{
-       completeTasks(){
-           return this.tasks.filter(task=>task.completed);
-
-        //    return this.tasks.filter(function(task){
-        //        return task.completed;
-        //    })
-       },
-
-       incompleteTasks(){
-            return this.tasks.filter(task=>!task.completed);
-
-            // return this.tasks.filter(function(task){
-            //     return !task.completed;
-            // });
-       }
     }
-
 });
+
+Vue.component('task',{
+    template:`<li><slot></slot></li>`
+})
+
+new Vue({
+    el:'#app'
+})
